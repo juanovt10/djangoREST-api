1a0ec44330a4c5c912b097e33bab97bec7fabbb9
Juan Velasquez <juanovelasquez10@gmail.com>
2024-01-21 11:51:29 +0000

    Attempt to fix likes bug

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,17 +1,21 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from likes.models import Like
+
 
 class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model
+    The create method handles the unique constraint on 'owner' and 'post'
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Like
-        fields = ['id', 'owner', 'created_at', 'post',]
+        fields = ['id', 'created_at', 'owner', 'post']
 
-    
     def create(self, validated_data):
-        try: 
+        try:
             return super().create(validated_data)
         except IntegrityError:
             raise serializers.ValidationError({
